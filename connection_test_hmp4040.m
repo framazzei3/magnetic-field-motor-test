e23e4255098d217559400cf02e4733f459decbaa
Francesco <107559885+framazzei3@users.noreply.github.com>
2026-05-12 09:17:33 +0200

    Update connection test script

--- connection_test_hmp4040.m
+++ connection_test_hmp4040.m
@@ -1,40 +1,39 @@
 % Clear environment
 clear; clc;
 
-% Porta da usare (COM9)
+% Port to use (COM9)
 port = "ASRL9::INSTR";
 fprintf('Trying port: %s\n', port);
 
 % Cleanup
 clear psu;
 
-% Connessione
+% Connection
 psu = visadev(port);
 psu.Timeout = 5;
-
 fprintf('Connected to %s. Testing communication...\n', port);
 
-% Test connessione
+% Connection test
 writeline(psu, "*IDN?");
 idn = readline(psu);
 fprintf('Device identification: %s\n', idn);
 
-% Imposta il voltaggio a 3.3 V sul canale 1
+% Set voltage to 3.3 V on channel 1
 voltage_set = 3.3;
 cmd_voltage = sprintf("VOLT %.2f, CH1", voltage_set);
 writeline(psu, cmd_voltage);
 fprintf('Voltage set to %.2f V on channel 1.\n', voltage_set);
 
-% Accende l'uscita del canale 1
+% Turn on channel 1 output
 writeline(psu, "OUTP ON, CH1");
 fprintf('Output of channel 1 is now ON.\n');
 
-% Attende 3 secondi prima di spegnere l'uscita
+% Wait 3 seconds before turning off the output
 pause(3);
 
-% (Opzionale) chiude il canale 1
+% (Optional) turn off channel 1
 writeline(psu, "OUTP OFF, CH1");
 
-% Spegne tutte le uscite della power supply
-writeline(psu, "OUTP:GEN OFF"); % oppure "OUTP OFF" a seconda del modello
+% Turn off all power supply outputs
+writeline(psu, "OUTP:GEN OFF"); % or "OUTP OFF" depending on the model
 fprintf('All outputs turned OFF.\n');
